76e88d451b762c9c297d95d931b8022c98f746af
justinwilliamsrva <justin.williams2@me.com>
2020-08-03 19:33:00 -0400

    minor

--- server.js
+++ server.js
@@ -74,12 +74,10 @@ app.get("/api/reservations", function (req, res) {
 
 // Create New Reservations- takes in JSON input
 app.post("/api/reservations", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
+   
     var newReservation = req.body;
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
     console.log(newReservation);
     reservations.push(newReservation);
     res.json(newReservation);
